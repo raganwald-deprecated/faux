36bdfa90f96f859fd20492941005ebd59aefea84
Reg Braithwaite <reg@braythwayt.com>
2011-04-25 21:34:45 -0400

    A little better

--- examples/backbone_form/javascripts/input_view.js
+++ examples/backbone_form/javascripts/input_view.js
@@ -9,12 +9,7 @@ globals.InputView = Backbone.View.extend({
   preview: function (event) {
     event.preventDefault();
     event.stopPropagation();
-    var form = $(event.target).parents('form').andSelf().filter('form');
-    var attrs = _.foldl(form.serializeArray(),
-      "acc, obj -> ((acc[obj.name] = obj.value) && acc) || acc".lambda(),
-      {}
-    );
-    this.options.controller.output(attrs);
+    this.options.controller.output({ text: event.target.text.value });
   }
   
 });
